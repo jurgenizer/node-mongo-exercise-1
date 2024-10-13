7e2c685302a7f1f8a4303fd8f63ef605e2dd912e
Jurgen Geitner <jurgen.geitner@gmail.com>
2024-10-13 11:05:20 +0200

    exercise 3

---
+++ index-exercise-3.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises')
+    .then(() => console.log('Connected to MongoDb...'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+    const courseSchema = new mongoose.Schema({
+        name: String,
+        author: String,
+        tags: [String],
+        date: Date,
+        isPublished: Boolean,
+        price: Number
+    });
+
+    // Compile schema into a model
+// PascalCase to name our classes, e.g., Course
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+    return courses = await Course
+    .find({ isPublished: true})
+    .or([
+        {price: {$gte: 15}},
+         {name: /.*by.*/i}
+        ])
+    .sort({ price: 1 }) 
+    .select({ name: 1, title: 1, price: 1}) 
+};
+
+async function run() {
+    const courses = await getCourses();
+    console.log(courses);
+  }
+  
+  run();
+  
